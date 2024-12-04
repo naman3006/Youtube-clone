4a6deb4dfa038627ae288dd23eea2ac0f4a5069e
Naman <namanpatel806@gmail.com>
2024-12-04 19:56:29 +0530

    Youtube-clone-folder

---
+++ youtube_clone-main/src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":
+            process.env.REACT_APP_YOUTUBE_API_KEY || "680c85f053msh5d4440193f2ce4ap1cf4f2jsn52aa7aec01fd",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
